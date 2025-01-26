68fb8f1bb1106b8941aab3caff40bd2d839be54f
❂ <duraki@users.noreply.github.com>
2025-01-26 07:11:00 +0100

    Update SettingText+SettingTextField to support icons ref _ when-original-devs-wont-merge-good-PRs/Setting#2
    This feature implements icon support in SettingText and SettingTextField, following the _ ext ref: aheze/Setting#42
    
    ref #2

--- Sources/Views/SettingTextField.swift
+++ Sources/Views/SettingTextField.swift
@@ -13,29 +13,37 @@ import SwiftUI
  */
 public struct SettingTextField: View, Setting {
     public var id: AnyHashable?
+    public var icon: SettingIcon?
     public var placeholder: String
     @Binding public var text: String
+    public var horizontalSpacing = CGFloat(12)
     public var verticalPadding = CGFloat(14)
     public var horizontalPadding: CGFloat? = nil
 
     public init(
         id: AnyHashable? = nil,
+        icon: SettingIcon? = nil,
         placeholder: String,
         text: Binding<String>,
+        horizontalSpacing: CGFloat = CGFloat(12),
         verticalPadding: CGFloat = CGFloat(14),
         horizontalPadding: CGFloat? = nil
     ) {
         self.id = id
+        self.icon = icon
         self.placeholder = placeholder
         self._text = text
+        self.horizontalSpacing = horizontalSpacing
         self.verticalPadding = verticalPadding
         self.horizontalPadding = horizontalPadding
     }
 
     public var body: some View {
         SettingTextFieldView(
+            icon: icon,
             placeholder: placeholder,
             text: $text,
+            horizontalSpacing: horizontalSpacing,
             verticalPadding: verticalPadding,
             horizontalPadding: horizontalPadding
         )
@@ -44,18 +52,26 @@ public struct SettingTextField: View, Setting {
 
 struct SettingTextFieldView: View {
     @Environment(\.edgePadding) var edgePadding
-    
+
+    var icon: SettingIcon?
     let placeholder: String
     @Binding var text: String
 
+    var horizontalSpacing = CGFloat(12)
     var verticalPadding = CGFloat(14)
     var horizontalPadding: CGFloat? = nil
 
     var body: some View {
-        TextField(placeholder, text: $text)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.vertical, verticalPadding)
-            .padding(.horizontal, horizontalPadding ?? edgePadding)
-            .accessibilityElement(children: .combine)
+        HStack(spacing: horizontalSpacing) {
+            if let icon {
+                SettingIconView(icon: icon)
+            }
+
+            TextField(placeholder, text: $text)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical, verticalPadding)
+        }
+        .padding(.horizontal, horizontalPadding ?? edgePadding)
+        .accessibilityElement(children: .combine)
     }
 }
